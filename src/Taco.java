69905121175547183c4a979d77380d329332858b
league <league@imac-5.attlocal.net>
2018-05-26 11:27:14 -0700

    Getters and Setters

---
+++ src/Taco.java
@@ -0,0 +1,20 @@
+
+public class Taco {
+private String meat = "none";
+private String sauce = "none";
+
+String getMeat() {
+	return meat;
+}
+String getSauce() {
+	return sauce;
+}
+void setMeat(String meat) {
+	this.meat= meat;
+}
+void setSauce(String sauce) {
+	this.sauce= sauce;
+}
+}
+
+
